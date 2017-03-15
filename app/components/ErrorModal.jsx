9e17a4958e36c28694d4c23799c56b9204e56f3b
Stephen Axtell <axtell.stephen@gmail.com>
2017-03-15 19:26:41 +0200

    Fixing foundation modal issue

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,4 +1,6 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactDOMServer = require('react-dom/server')
 
 
 var ErrorModal = React.createClass({
@@ -13,13 +15,10 @@ var ErrorModal = React.createClass({
     message: React.PropTypes.string.isRequired
   },
   componentDidMount: function () { //a provided method to do something after everythin is bootstrapped together
-    var modal = new Foundation.Reveal($('#error-modal'));//new instance of Foundation Modal which is done after component is created
-    modal.open();
-  },
-  render: function () {
+
     var {title,message} = this.props;
 
-    return (
+    var modalMarkup = (
       <div id="error-modal" className="reveal tiny text-center" data-reveal="">
         <h4>{title}</h4>
         <p>{message}</p>
@@ -30,6 +29,21 @@ var ErrorModal = React.createClass({
         </p>
       </div>
     );
+
+    var $modal = $(ReactDOMServer.renderToString(modalMarkup));
+    $(ReactDOM.findDOMNode(this)).html($modal);
+
+    var modal = new Foundation.Reveal($('#error-modal'));//new instance of Foundation Modal which is done after component is created
+    modal.open();
+  },
+  render: function () {
+
+
+    return (
+      <div></div>
+    );
+
+
   }
 
 });
